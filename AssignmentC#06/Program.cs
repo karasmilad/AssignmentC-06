cbbce7c75d6886518a179cbe3b700ccf30769a6f
karasmilad <elpopkaras12@gmail.com>
2025-07-16 16:43:51 +0300

    Question01

--- AssignmentC#06/Program.cs
+++ AssignmentC#06/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Question01
+            //Value Type
+            //1)Passing by value means that a copy of the variable is passed to the method.
+            //2)Passing by reference means that a reference to the variable is passed to the method.
+            #endregion
+
+
         }
     }
 }
