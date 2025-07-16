6f52158fb528250b87f9f99f609fb624e7acf323
karasmilad <elpopkaras12@gmail.com>
2025-07-16 18:36:05 +0300

    Question04

--- AssignmentC#06/Program.cs
+++ AssignmentC#06/Program.cs
@@ -9,6 +9,16 @@
             Console.WriteLine($"The Summation is : {summation}");
             Console.WriteLine($"The Subtracting is : {subtracting}");
         }
+        static int SumOfDigits(int number)
+        {
+            int sum = 0;
+            while (number > 0)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+            return sum;
+        }
         static void Main(string[] args)
         {
             #region Question01
@@ -22,7 +32,17 @@
             //2)Passing by reference means that a reference to the reference of the object is passed to the method.
             #endregion
             #region Question03
-            calculator(5,10,20,10);
+            //calculator(5,10,20,10);
+            #endregion
+            #region Question04
+            int input;
+            do
+            {
+                Console.Write("Enter a number: ");
+            }
+            while (int.TryParse(Console.ReadLine(), out input));
+            int result = SumOfDigits(input);
+            Console.WriteLine($"The sum of the digits of the number {input} is: {result}");
             #endregion
         }
     }
