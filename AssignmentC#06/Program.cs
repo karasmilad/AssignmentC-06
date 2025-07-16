45cfa2a84d81fdfd809ffc88cb9ad53d11c87f6d
karasmilad <elpopkaras12@gmail.com>
2025-07-16 16:44:51 +0300

    Question02

--- AssignmentC#06/Program.cs
+++ AssignmentC#06/Program.cs
@@ -9,8 +9,11 @@
             //1)Passing by value means that a copy of the variable is passed to the method.
             //2)Passing by reference means that a reference to the variable is passed to the method.
             #endregion
-
-
+            #region Question02
+            //Reference Type
+            //1)Passing by value means that a copy of the reference to the object is passed to the method.
+            //2)Passing by reference means that a reference to the reference of the object is passed to the method.
+            #endregion
         }
     }
 }
