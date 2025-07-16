454dc6dee4169486ac00b91bc5ee6c501098c2e5
karasmilad <elpopkaras12@gmail.com>
2025-07-16 17:27:25 +0300

    Question03

--- AssignmentC#06/Program.cs
+++ AssignmentC#06/Program.cs
@@ -2,6 +2,13 @@
 {
     internal class Program
     {
+        public static void calculator(int a, int b, int c, int d)
+        {
+            int summation = a + b;
+            int subtracting = c - d;
+            Console.WriteLine($"The Summation is : {summation}");
+            Console.WriteLine($"The Subtracting is : {subtracting}");
+        }
         static void Main(string[] args)
         {
             #region Question01
@@ -13,6 +20,9 @@
             //Reference Type
             //1)Passing by value means that a copy of the reference to the object is passed to the method.
             //2)Passing by reference means that a reference to the reference of the object is passed to the method.
+            #endregion
+            #region Question03
+            calculator(5,10,20,10);
             #endregion
         }
     }
